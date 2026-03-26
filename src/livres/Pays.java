dada6f7514893787295d63ad40117be952217fbd
Leonard Cote <2241670@cegeplimoilou.ca>
2026-03-26 13:46:01 -0400

    commitThree

--- src/livres/Pays.java
+++ src/livres/Pays.java
@@ -1,4 +1,44 @@
 package livres;
 
 public class Pays {
+    private String codePays;
+    private String nom;
+
+    public String getCodePays() {
+        return codePays;
+    }
+
+    public void setCodePays(String codePays) {
+        ValiderPays(codePays);
+    }
+
+    public Pays (String codePays, String nom){
+        ValiderPays(codePays);
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    private void ValiderPays(String codePays) {
+        if (IsValidPays(codePays)) {
+            this.codePays = codePays;
+        } else {
+            this.codePays = "AAA";
+        }
+    }
+
+    private boolean IsValidPays(String codePays){
+        return codePays.length() == 3 && codePays.toUpperCase().equals(codePays);
+    }
+
+    @Override
+    public String toString() {
+        return nom;
+    }
 }
--- src/livres/Serie.java
+++ src/livres/Serie.java
@@ -1,4 +1,37 @@
 package livres;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Serie {
+    private String nom;
+    List<Ouvrage> listeOuvrages = new ArrayList<>();
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public List<Ouvrage> getListeOuvrages() {
+        return listeOuvrages;
+    }
+
+    public void setListeOuvrages(List<Ouvrage> listeOuvrages) {
+        this.listeOuvrages = listeOuvrages;
+    }
+
+    public void ajouterOuvrage(Ouvrage ouvrageAAjouter){
+        listeOuvrages.add(ouvrageAAjouter);
+    }
+
+    public void enleverOuvrage(Ouvrage ouvreageAEnlever){
+        listeOuvrages.remove(ouvreageAEnlever);
+    }
+
+    public Serie(String nom){
+        this.nom = nom;
+    }
 }
